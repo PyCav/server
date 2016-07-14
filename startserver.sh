f28b71a7e85dc724fa1916b2b9a0dec58ed89b18
Jordan Osborn <jordanosborn0@gmail.com>
2016-07-14 17:11:03 +0100

    added

---
+++ startserver.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+cd /home/public/Server
+function run {
+			   sudo updatecontainers.sh &
+			   sudo setupcontainers.sh &
+			   sudo killidlecontainers.sh &
+               sudo jupyterhub
+           }
+
+until run; do
+    echo "Server 'jupyter' crashed with exit code $?.  Respawning.." >&2
+    sleep 1
+done
