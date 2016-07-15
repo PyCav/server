abc98bdca335ce32341ffc46defaef6f829eba24
Jordan Osborn <jordanosborn0@gmail.com>
2016-07-15 15:29:59 +0100

    changed pip commands

--- setupcontainers.sh
+++ setupcontainers.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+i=0
 while true; do
         sleep 1
         containers=$(sudo docker ps -a | awk '{if(NR>1) print $NF}')
@@ -8,10 +9,10 @@ while true; do
                         i=$((i+1))
                         if (( i > iold )) ; then
                                 newcontainer=$container
-                                sudo docker exec $newcontainer pip3 install pycav
-                                sudo docker exec $newcontainer pip3 install vpython
-                                sudo docker exec $newcontainer pip install pycav
-                                sudo docker exec $newcontainer pip install vpython
+                                sudo docker exec $newcontainer pip3 install --upgrade pycav
+                                sudo docker exec $newcontainer pip3 install --upgrade vpython
+                                sudo docker exec $newcontainer pip install --upgrade pycav
+                                sudo docker exec $newcontainer pip install --upgrade vpython
                                 echo $newcontainer
                         fi
         done
