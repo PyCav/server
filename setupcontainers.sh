70833e91463a96838a8b5f430f9a6d4b928ae030
Jordan Osborn <jordanosborn0@gmail.com>
2016-07-15 15:28:18 +0100

    commit

--- setupcontainers.sh
+++ setupcontainers.sh
@@ -1,21 +1,18 @@
-#!bin/bash
-
-
-i=0
+#!/bin/bash
 while true; do
-	sleep(1):
-	containers=$(sudo docker ps -a | awk '{if(NR>1) print $NF}')
-	iold=i
-	i=0
-	for container in $containers; do
-			i=$((i+1))
-			if [ $i -gt $iold ]; then
-				newcontainer=$container
-				sudo docker exec $newcontainer pip3 install pycav
-				sudo docker exec $newcontainer pip3 install vpython
-				sudo docker exec $newcontainer pip install pycav
-				sudo docker exec $newcontainer pips install vpython
-				echo $newcontainer
-			fi
-	done
-done		
+        sleep 1
+        containers=$(sudo docker ps -a | awk '{if(NR>1) print $NF}')
+        iold=$i
+        i=0
+        for container in $containers; do
+                        i=$((i+1))
+                        if (( i > iold )) ; then
+                                newcontainer=$container
+                                sudo docker exec $newcontainer pip3 install pycav
+                                sudo docker exec $newcontainer pip3 install vpython
+                                sudo docker exec $newcontainer pip install pycav
+                                sudo docker exec $newcontainer pip install vpython
+                                echo $newcontainer
+                        fi
+        done
+done
