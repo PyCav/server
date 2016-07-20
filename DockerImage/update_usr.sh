5c7e62a5a4e558a3447ac4547c2ee519e0fe1d46
Jordan Osborn <jordanosborn0@gmail.com>
2016-07-20 16:45:12 +0100

    changed

--- DockerImage/update_usr.sh
+++ DockerImage/update_usr.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-set -e
-exec usermod -l $JPY_USER jovyan
+#set -e
+#exec usermod -l $JPY_USER jovyan
 #exec su $JPY_USER
+
+set -e
+useradd $JPY_USER -d /home/jovyan
+echo $JPY_USER:jupyter | chpasswd
+userdel jovyan
+su $JPY_USER
