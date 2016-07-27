2452ac9ae3787f94a30a783f6af8aff8f5a41317
jordanosborn <jordanosborn0@gmail.com>
2016-07-27 10:42:24 +0100

    configures apache config to AllowOverride FileInfo in /var/www/

---
+++ configure_apache.py
@@ -0,0 +1,22 @@
+try:
+	foundLine=False
+	with open("/etc/apache2/apache2.conf",'r') as f:
+		contents = f.readlines()
+	
+	for i in range(0,len(contents)):
+		if contents[i].find("<Directory /var/www/>") != -1 :
+			foundLine=True
+		if foundLine:
+			if contents[i].find("AllowOverride None") != -1:
+				contents[i]="\tAllowOverride FileInfo\n"
+				break
+
+	apacheConf=open("/etc/apache2/apache2.conf",'w')	
+	for line in contents:
+		apacheConf.write(line)
+	apacheConf.close()
+
+	print("Apache config file successfully edited.")
+
+except:
+	print("Couldn't edit Apache config file.'")
