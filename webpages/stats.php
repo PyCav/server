2c7593da5a9ba4260d4752db26c6f9c12b378fb8
jordanosborn <jordanosborn0@gmail.com>
2016-07-29 15:22:22 +0100

    fixed layout

--- webpages/stats.php
+++ webpages/stats.php
@@ -1,11 +1,10 @@
 <?php
-echo nl2br( file_get_contents('/home/public/server/stats.txt') );
+	echo nl2br( file_get_contents('/home/public/server/stats.txt') );
 ?>
 <html>
-<body>
-<p>
-<a href="/index.php"> back </a>
-</p>
-</body>
-
+	<body>
+		<p>
+			<a href="/index.php"> back </a>
+		</p>
+	</body>
 </html>
