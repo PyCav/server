17b0559086fa28aa52f779b8b0e6a36201316701
jordanosborn <jordanosborn0@gmail.com>
2016-07-29 11:50:53 +0100

    now kills server before running

---
+++ updatecontainers.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#update dockerimage
+#stop server before running
+echo "Stopping server"
+pkill -f startServer
+echo "Stopping and removing all containers"
+removecontainers
+# $1 build flag -b / -p pull docker hub , $2 docker image name, $3 /path/to/Dockerfile
+if [ "$1" -eq "-b" ]; then
+	echo "Removing old image"
+	docker rmi $2
+	echo "Getting new image"
+	docker build -t $2:latest $3
+elif [ "$1" -eq "-p" ]; then
+	echo "Removing old image"
+	docker rmi $2
+	echo "Getting new image"
+	docker pull $2
+else
+	echo "Removing old image"
+	docker rmi jordanosborn/pycav
+	echo "Getting new image"
+	docker pull jordanosborn/pycav
+fi
