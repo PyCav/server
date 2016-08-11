e5a46fe5e453e1cedc1a61321356c09342504caa
jordanosborn <jordanosborn0@gmail.com>
2016-08-11 12:08:47 +0100

    now uses relative paths and calls setcustomparent.sh

--- updatescripts.sh
+++ updatescripts.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /home/public/server
+
 #replace domain with site_name and PORT with port
 echo "Script assumes you are using raven authentication."
 echo "Script will also overwrite any changes you have made to files in your server directory."
@@ -11,19 +12,20 @@ if [ "$ans" == "y" ]; then
 	site_name='domain'
 	port='PORT'
 	#raven r , github g, local user l flags?
-	sudo sed -i -- 's/raven = False/raven = True/g' /home/public/server/jupyterhub_config.py
-	sudo sed -i -- 's/website/'$site_name'/g' /home/public/server/jupyterhub_config.py
-	sudo sed -i -- 's/8000/'$port'/g' /home/public/server/jupyterhub_config.py
-	sudo sed -i -- 's/auth_key='\'\''/auth_key='\'$CONFIGPROXY_AUTH_TOKEN\''/g' /home/public/server/jupyterhub_config.py
-	sudo sed -i -- 's/#demos_//g' /home/public/server/jupyterhub_config.py
+	sudo sed -i -- 's/raven = False/raven = True/g' ./jupyterhub_config.py
+	sudo sed -i -- 's/website/'$site_name'/g' ./jupyterhub_config.py
+	sudo sed -i -- 's/8000/'$port'/g' ./jupyterhub_config.py
+	sudo sed -i -- 's/auth_key='\'\''/auth_key='\'$CONFIGPROXY_AUTH_TOKEN\''/g' ./jupyterhub_config.py
+	sudo sed -i -- 's/#demos_//g' ./jupyterhub_config.py
 	sudo updatescripts_subscript $site_name $port
+	sudo ./setcustomparent.sh
 	sudo chmod a+x *.sh
 	sudo cp ./updatescripts_subscript.sh /usr/local/bin/updatescripts_subscript
 	sudo cp ./updatescripts.sh /usr/local/bin/updatescripts
-	sudo cp /home/public/server/startserver.sh /usr/local/bin/startserver
-	sudo cp /home/public/server/killserver.sh /usr/local/bin/killserver
-	sudo cp /home/public/server/removecontainers.sh /usr/local/bin/removecontainers
-	sudo cp /home/public/server/updatecontainers.sh /usr/local/bin/updatecontainers
+	sudo cp ./startserver.sh /usr/local/bin/startserver
+	sudo cp ./killserver.sh /usr/local/bin/killserver
+	sudo cp ./removecontainers.sh /usr/local/bin/removecontainers
+	sudo cp ./updatecontainers.sh /usr/local/bin/updatecontainers
 fi
 sudo runuser -l $USER -c 'source ~/.bashrc'
 echo "Script Complete"
