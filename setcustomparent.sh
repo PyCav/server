1ef5119f02c019913777411781bdeb4f9ae65e42
jordanosborn <jordanosborn0@gmail.com>
2016-08-11 15:57:17 +0100

    fixed seds

--- setcustomparent.sh
+++ setcustomparent.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #set custom parent directory
 
+
 cd "${0%/*}"
 cd ..
 path=`pwd`
-sudo sed -i -- 's/\/home\/public/'$path'/g' ./jupyterhub_config.py
-cd server
-sudo sed -i -- 's/\/home\/public/'$path'/g' ./startserver.sh
-sudo sed -i -- 's/\/home\/public/'$path'/g' ./updatescripts.sh
-sudo sed -i -- 's/\/home\/public/'$path'/g' ./updatescripts_subscript.sh
+sudo sed -i -- 's:/home/public:'$path':g' ./server/jupyterhub_config.py
+sudo sed -i -- 's:/home/public:'$path':g' ./server/startserver.sh
+sudo sed -i -- 's:/home/public:'$path':g' ./server/updatescripts.sh
+sudo sed -i -- 's:/home/public:'$path':g' ./server/updatescripts_subscript.sh
