b4ac09591162b0e5dfc24c4947a62118ff478c81
jordanosborn <jordanosborn0@gmail.com>
2016-08-11 11:18:34 +0100

    now using relative paths

--- cron/updatenotebooks.sh
+++ cron/updatenotebooks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cd "${0%/*}"
 #add to roots cron ?
 rm -R /home/public/demos
 rm -R /home/public/data
@@ -7,7 +7,4 @@ rm -R /home/public/investigations
 git clone https://github.com/pycav/demos.git /home/public/demos
 git clone https://github.com/pycav/data.git /home/public/data
 git clone https://github.com/pycav/investigations.git /home/public/investigations
-cd /home/public/server/python/
-python3 index_generator.py -p /home/public/demos -t "PyCav Demos Index"
-python3 index_generator.py -p /home/public/data -t "PyCav Data Notebooks"
-python3 index_generator.py -p /home/public/investigations -t "PyCav Investigations Notebooks"
+./indexgen.sh
