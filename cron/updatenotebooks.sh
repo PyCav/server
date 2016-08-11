34b222aa1ddb6ef4e73439d154ae82eae62bdbb4
jordanosborn <jordanosborn0@gmail.com>
2016-08-11 10:31:51 +0100

    added data and investigation repos and generation of index notebooks for each notebook directory

--- cron/updatenotebooks.sh
+++ cron/updatenotebooks.sh
@@ -2,4 +2,12 @@
 
 #add to roots cron ?
 rm -R /home/public/demos
+rm -R /home/public/data
+rm -R /home/public/investigations
 git clone https://github.com/pycav/demos.git /home/public/demos
+git clone https://github.com/pycav/data.git /home/public/data
+git clone https://github.com/pycav/investigations.git /home/public/investigations
+cd /home/public/server/python/
+python3 index_generator.py -p /home/public/demos -t "PyCav Demos Index"
+python3 index_generator.py -p /home/public/data -t "PyCav Data Notebooks"
+python3 index_generator.py -p /home/public/investigations -t "PyCav Investigations Notebooks"
