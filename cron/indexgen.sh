3c2172b16dfa4972e1672350edb07618d18ab176
jordanosborn <jordanosborn0@gmail.com>
2016-08-12 12:24:36 +0100

    now renames indexgen.ipynb to index.ipynb for each repo

--- cron/indexgen.sh
+++ cron/indexgen.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cd "${0%/*}"
 cd ../python/
+
 python3 ./index_generator.py -p /home/public/demos -t "PyCav Demos Index"
 python3 ./index_generator.py -p /home/public/data -t "PyCav Data Notebooks"
 python3 ./index_generator.py -p /home/public/investigations -t "PyCav Investigations Notebooks"
+
+mv /home/public/demos/indexgen.ipynb /home/public/demos/index.ipynb
+mv /home/public/data/indexgen.ipynb /home/public/data/index.ipynb
+mv /home/public/investigations/indexgen.ipynb /home/public/investigations/index.ipynb 
