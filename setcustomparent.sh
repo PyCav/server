f856afa7da59b7cfb59404e921a9830ae9ac2638
jordanosborn <jordanosborn0@gmail.com>
2016-08-12 12:26:50 +0100

    fixes custom parent for cron scripts

--- setcustomparent.sh
+++ setcustomparent.sh
@@ -9,3 +9,7 @@ sudo sed -i -- 's:/home/public:'$path':g' ./server/jupyterhub_config.py
 sudo sed -i -- 's:/home/public:'$path':g' ./server/startserver.sh
 sudo sed -i -- 's:/home/public:'$path':g' ./server/updatescripts.sh
 sudo sed -i -- 's:/home/public:'$path':g' ./server/updatescripts_subscript.sh
+
+sudo sed -i -- 's:/home/public:'$path':g' ./server/cron/backup.sh
+sudo sed -i -- 's:/home/public:'$path':g' ./server/cron/indexgen.sh
+sudo sed -i -- 's:/home/public:'$path':g' ./server/cron/updatenotebooks.sh
