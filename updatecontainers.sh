d06e8e0e9d543bb805e1e99067cc41881d8dfe83
jordanosborn <jordanosborn0@gmail.com>
2016-08-16 12:33:11 +0100

    made script use sensible defaults if arguments aren’t provided

--- updatecontainers.sh
+++ updatecontainers.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #update dockerimage
 #stop server before running
+cd /home/public/server
 echo "Stopping server"
 killserver
 echo "Stopping and removing all containers"
@@ -8,14 +9,32 @@ removecontainers
 # $1 build flag -b / -p pull docker hub , $2 docker image name, $3 /path/to/Dockerfile
 if [ "$1" == "-b" ]; then
 	echo "Removing old image"
-	docker rmi $2
-	echo "Getting new image"
-	docker build -t $2:latest $3
+	if [ $# -eq 1 ]; then
+		docker rmi jordanosborn/pycav
+		echo "Building new image"
+		docker build -t  jordanosborn/pycav:latest ./DockerImage/
+	else
+		if [ $# -eq 2 ]; then
+			docker rmi $2
+			echo "Building new image"
+			docker build -t $2:latest ./DockerImage/
+		else
+			docker rmi $2
+			echo "Building new image"
+			docker build -t $2:latest $3
+		fi
+	fi
 elif [ "$1" == "-p" ]; then
 	echo "Removing old image"
-	docker rmi $2
-	echo "Getting new image"
-	docker pull $2
+	if [ $# -eq 1 ]; then
+		docker rmi jordanosborn/pycav
+		echo "Getting new image"
+		docker pull jordanosborn/pycav
+	else
+		docker rmi $2
+		echo "Getting new image"
+		docker pull $2
+	fi
 else
 	echo "Removing old image"
 	docker rmi jordanosborn/pycav
