0b9c4370cc341a314f994264f49e7a8bb67f7bf9
jordanosborn <jordanosborn0@gmail.com>
2016-08-19 14:24:22 +0100

    added build status using —status flag

--- global/triggerbuild.sh
+++ global/triggerbuild.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/jordanosborn/pycav/trigger/c0f580de-d805-4816-b12e-ab2b144eed7a/
-echo $'\nBuild Triggered, ~30 mins to complete.'
+cd /home/public/
+if [ "$1" == "--status" ]; then
+	python3 ./server/python/buildstatus.py
+else
+	curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/jordanosborn/pycav/trigger/c0f580de-d805-4816-b12e-ab2b144eed7a/
+	echo $'\nBuild Triggered, ~30 mins to complete.'
+fi
