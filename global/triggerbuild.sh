a590dbac47cbc6c930871c64db9330a30d8c5ed8
Jordan Osborn <jordanosborn@users.noreply.github.com>
2016-08-19 09:29:29 +0100

    Update triggerbuild.sh

--- global/triggerbuild.sh
+++ global/triggerbuild.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/jordanosborn/pycav/trigger/c0f580de-d805-4816-b12e-ab2b144eed7a/
-echo $'\nBuild Triggered, ~30 mins to complete.'
-echo "Go to https://hub.docker.com/r/jordanosborn/pycav/builds to see build progress."
+if [ "$1" != "--quiet"]; then
+  echo $'\nBuild Triggered, ~30 mins to complete.'
+  echo "Go to https://hub.docker.com/r/jordanosborn/pycav/builds to see build progress."
+fi
